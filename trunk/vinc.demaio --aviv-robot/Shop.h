4908a9bacb4ab589b40b810ce9c07b542232bc24
vinc.demaio <vinc.demaio@906796ec-c859-11de-8672-e51b9359e43c>
2009-11-12 08:46:18 +0000

    Added a file remotely

---
+++ trunk/vinc.demaio --aviv-robot/Shop.h
@@ -0,0 +1,36 @@
+#include <string>
+
+class Shop {
+private:
+        string name;
+        vector<Module> products;
+public:
+        Shop(string nome, vector<Module> products);
+        ~Shop();
+        vector<Module> getProductList();
+        Module getModule(string nome);
+};
+
+Shop::Shop(std::string nome, vector<Module> products){
+	this->name = name;
+	this->products = products;
+}
+
+Shop::~Shop(){
+	products.~vector();
+}
+
+vector<Module> Shop::getProductList(){
+	return products;
+}
+
+Module Shop::getModule(string nome){
+	vector<Module>::iterator it = this->products.begin();
+	while(it != this->products.end()){
+		if(it->getName() == nome) {
+			return *it;
+			break;
+		}
+		++it;
+	}
+}
